666afdbe38cc3e2a57e19e649863d4f96e854eb1
Austin N <aurum27@tamu.edu>
2020-02-06 21:32:30 +0000

    Part 1 Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:order])
+    @order = params[:order]
   end
 
   def new
